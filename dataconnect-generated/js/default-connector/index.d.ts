7ad52aa1cb1d1b9b6c98de35257805ac4c493ccc
HassanBahati <mukisabahati@gmail.com>
2025-07-30 18:46:32 +0300

    feat: generate sdk

---
+++ dataconnect-generated/js/default-connector/index.d.ts
@@ -0,0 +1,17 @@
+import { ConnectorConfig } from 'firebase/data-connect';
+
+export const connectorConfig: ConnectorConfig;
+
+export type TimestampString = string;
+export type UUIDString = string;
+export type Int64String = string;
+export type DateString = string;
+
+
+
+
+export interface Movie_Key {
+  id: UUIDString;
+  __typename?: 'Movie_Key';
+}
+
