d33f324090fd650ec41e0e653b51b4c6e6df03ed
HassanBahati <mukisabahati@gmail.com>
2025-07-30 18:53:28 +0300

    docs: add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Environments 
+firebase-tools: 14.11.0
+node.js: 22.6.0
---
+++ dataconnect/connector/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'dataconnect-firebase-v12-demo',
+  location: 'us-central1'
+};
+
